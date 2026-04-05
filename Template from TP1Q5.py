ec6b5a3f58c5e34115c433b287ad50b3a707419e
throwupandthrowaway <itsjohnmail04@gmail.com>
2026-04-05 19:05:52 +0200

    Add files via upload

---
+++ Template from TP1Q5.py
@@ -0,0 +1,36 @@
+#Example: $\frac{d^2x}{dt^2}=-\omega_0^2x$
+
+import numpy as np
+def deriv(t,y,params):  #Separate the 2nd order ODE into 2 1st order ODEs
+    dy=np.zeros(y.size)
+    omega=params
+    dy[0]=y[1]  #y=dx/dt
+    dy[1]=-omega**2*y[0]    #dy/dt=-omega**2x
+    return dy
+
+#You don't need to touch that it's already taken care of
+#Just make sure euler.py is in the same directory as your script
+from euler import euler
+omega=1
+t=0
+dt=0.01 #Time step
+Tmax=20
+Tvalues=np.arange(0,Tmax,dt)
+n=Tvalues.size
+xvalues=np.empty(n) #Position curve
+vvalues=np.empty(n) #Velocity curve
+x0=1    #Initial values
+v0=0
+y=np.empty(2)   #Create a vector containing both curves
+y[0]=x0
+y[1]=v0
+
+for i in range(n):
+    xvalues[i]=y[0]
+    vvalues[i]=y[1]
+    t=Tvalues[i]
+    y=euler(t,dt,y,deriv,omega)
+    
+import matplotlib.pyplot as plt
+plt.plot(Tvalues,xvalues,label="x(t)")
+plt.plot(Tvalues,vvalues,label="v(t)")
